96f939dfb09ffd37e4b33bc51a9a95d89fe500bf
Fathan Rohman <karir.fathan@gmail.com>
2024-02-02 14:30:09 +0700

    add: revision config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  moduleFileExtensions: ["js", "json", "vue"],
   "transform": {
     ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
